71f1a8178a5aa1eb09ff27dec8f47ac7bf08dbad
muhammadhassan2 <36268516+muhammadhassan2@users.noreply.github.com>
2024-08-25 23:39:18 +0300

    update

--- linkedlist/linkedlist.cpp
+++ linkedlist/linkedlist.cpp
@@ -7,11 +7,63 @@ linkedlist<T>::linkedlist(T data):mData{data}
 
 template<class T>
 void linkedlist<T>::insertData(T data){
+    std::cout<<"InsertData function\n";
     Node<T> *tempNode = new Node(data);
     tempNode->next = this->Head;
     this->Head = tempNode;
+}
 
+template<class T>
+void linkedlist<T>::deleteData(T data){
+    std::cout<<"deleteData function\n";
+    Node<T>* tempList = this->Head;
+    Node<T>* prevPart = this->Head;
 
+    if(tempList->mData == data){
+        this->Head = this->Head->next; 
+        delete tempList;
+        return ;
+    }
+
+    while(tempList->mData != data && tempList != nullptr){
+         prevPart = tempList; 
+         tempList = tempList->next;
+    }
+
+    if(tempList == nullptr){
+        std::cout<<"the Value is not fount in the list\n";
+        return ;
+    }
+     
+    prevPart->next = tempList->next;
+    std::cout<<"the key is deleted from the list\n";
+    delete tempList;
+    delete prevPart;
+    return ; 
+}
+template<class T>
+bool linkedlist<T>::isExist(T value){
+    
+}
+template<class T>
+bool linkedlist<T>::isEmpty(){
+     std::cout<<"isEmpty function\n";
+     return (this->getSize() == 0):True ? false ; 
+}
+template<class T>
+void linkedlist<T>::printList(){
+      std::cout<<"printList function\n";
+      Node<T>* tempList = this->Head;
+      while(tempList){
+
+        std::cout<<tempList->mData<<" \n";
+        tempList = tempList->next;
+      }
+      
+}
+template<class T>
+size_t linkedlist<T>::getSize(){
+    return this->_size;
 }
 template<class T>
 linkedlist<T>::~linkedlist()
--- linkedlist/linkedlist.hpp
+++ linkedlist/linkedlist.hpp
@@ -1,11 +1,35 @@
 
 #pragma onece
 
+template<class T>
+struct Node{
+    T mData;
+    Node<T>* next;
+    Node():mData{0},next{nullptr} {
+        std::cout<<"Node struct No-args constractor\n";
+    }
+
+    Node(T data):mData{data},next{nullptr}{
+        std::cout<<"Node struact 1-args constractor\n";
+    }
+};
+
+template<class T>
 class linkedlist
 {
 private:
-    /* data */
+    Node<T>*Head;
+    size_t _size;
 public:
-    linkedlist(/* args */);
+    linkedlist(T data);
+    void insertData(T data);
+    void deleteData(T data);
+    bool isExist(T value);
+    bool isEmpty();
+    void printList();
+    size_t getSize();
+
+
     ~linkedlist();
+
 };
